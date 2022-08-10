880fbc15e72c34d00a6cd9e4ffc324194b71ff94
Damdinsuren Dorjsuren <damdinsuren@live.com>
2022-08-10 16:20:30 +0800

    udpate core

--- src/pages/admin/categories.tsx
+++ src/pages/admin/categories.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import Category from '../../forms/Category/Category';
+import dynamic from 'next/dynamic';
+import { Spinner } from 'nuudel-core';
+
+const DynamicCategory = dynamic(() => import('../../forms/Category/Category'), {
+  ssr: false,
+  loading: () => <Spinner />,
+});
 
 type Props = {};
 
 const Categories: React.FC<Props> = () => {
-  return <Category />;
+  return <DynamicCategory />;
 };
 
 export default Categories;
