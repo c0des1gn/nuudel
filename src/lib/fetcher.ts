106c9bc9a9821fca125edf27886c62d759483c6f
Damdinsuren Dorjsuren <damdinsuren@live.com>
2022-09-14 14:51:14 +0800

    fetch

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,16 +1,38 @@
 import axios from 'axios';
-import { UI } from 'nuudel-core';
+import {UI} from 'nuudel-core';
+import {isServer} from 'nuudel-utils';
+import type { SWRConfiguration } from 'swr'
 
-const { WEB = '' } = process.env;
+const {WEB = ''} = process.env;
 const pathname: string = 'api/graphql';
 
-export const fetcher = async (query: string, variables: any = {}) =>
+export const fetcher = async (
+  query: string,
+  variables: any = {},
+  headers: any = {},
+) =>
   axios({
-    url: `${WEB}/${pathname}`,
+    url: `${WEB || process?.env?.WEB}/${pathname}`,
     method: 'post',
     data: {
       query: query,
       variables: variables,
     },
-    headers: { ...(await UI.headers()) },
+    headers: {...(await UI.headers()), ...headers},
   }).then(res => res.data);
+
+export const swrOptions: SWRConfiguration = {
+  //revalidateOnMount: false,
+  revalidateOnFocus: false,
+  revalidateIfStale: false,
+  revalidateOnReconnect: false,
+  loadingTimeout: 10000,
+  focusThrottleInterval: 0,
+  dedupingInterval: 10000,
+  errorRetryInterval: 10000,
+  errorRetryCount: 1,
+  shouldRetryOnError: false,
+  //refreshWhenHidden: false,
+  //refreshWhenOffline: false,
+  //refreshInterval: 0,
+};
